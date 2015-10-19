636ba77df4a7dbeb57013dbc76f57eadf860675e
Richard Ejem <richard@ejem.cz>
2015-10-19 23:42:10 +0200

    used auto run-tests script

---
+++ tests/run-tests.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+pushd "$(dirname "$0")/.."
+
+cp ./tests/php-unix.ini ./tests/php.ini
+PHP_EXT=`php -r "echo ini_get('extension_dir');"`
+echo "" >> ./tests/php.ini # empty line
+echo "extension_dir=$PHP_EXT" >> ./tests/php.ini
+
+./vendor/bin/tester ./tests/$1 -p php -c ./tests
+
+popd
